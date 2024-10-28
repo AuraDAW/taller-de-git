5347fef1ea4cfddc5d519038391a3640cd525ca6
Aura Guerrero <aguemar696@g.educaand.es>
2024-10-28 14:28:51 +0100

    Arreglado error en clase hola

--- lib/hola.php
+++ lib/hola.php
@@ -2,7 +2,13 @@
 <<<<<<< HEAD
 // Autor: Manuel Paniagua <mpaniagua@iestrassierra.com>
 require('HolaMundo.php');
+<<<<<<< HEAD
 print "Introduce tu nombre:";
 $nombre = trim(fgets(STDINN));
 @print new HolaMundo($nombre);
 ?>
+=======
+
+$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+print new HolaMundo($nombre);
+>>>>>>> b8a6e6b (Arreglado error en clase hola)
